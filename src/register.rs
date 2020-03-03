845c92b5ef2b0ac4d392b685dc27920fd5cd373c
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-03-03 10:54:05 +0900

    Add getters and setters for Register

--- src/register.rs
+++ src/register.rs
@@ -4,3 +4,51 @@ pub struct Register {
     pub carry_flag: u8, // carry flag
     pub pc: u8, // program counter
 }
+
+impl Register {
+
+    pub fn new() -> Self {
+        Self {
+            register_a: 0,
+            register_b: 0,
+            carry_flag: 0,
+            pc: 0
+        }
+    }
+
+    pub fn pc(&self) -> u8 {
+        self.pc
+    }
+
+    pub fn set_pc(&mut self, new_value: u8) {
+        self.pc = new_value;
+    }
+
+    pub fn incr_pc(&mut self) {
+        self.pc += 1;
+    }
+
+    pub fn carry_flag(&self) -> u8 {
+        self.carry_flag
+    }
+
+    pub fn set_carry_flag(&mut self, new_value: u8) {
+        self.carry_flag = new_value;
+    }
+
+    pub fn register_a(&self) -> u8 {
+        self.register_a
+    }
+
+    pub fn set_register_a(&mut self, new_value: u8) {
+        self.register_a = new_value;
+    }
+
+    pub fn register_b(&self) -> u8 {
+        self.register_b
+    }
+
+    pub fn set_register_b(&mut self, new_value: u8) {
+        self.register_b = new_value;
+    }
+}
