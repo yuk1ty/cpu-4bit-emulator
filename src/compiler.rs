8ce4bb7d672f6a3ab5fcf3db6368704011a1620e
yuk1ty <yuki.mul.tiplus@gmail.com>
2020-03-28 20:30:29 +0900

    Add other operations

--- src/compiler.rs
+++ src/compiler.rs
@@ -12,24 +12,21 @@ impl Compiler {
         let mut result = Vec::new();
 
         for token in tokens {
-            match token {
-                Token::Mov(Register::A, im) => {
-                    result.push(self.gen_bin_code(0b0011, im)?);
-                }
-                Token::Mov(Register::B, im) => {
-                    result.push(self.gen_bin_code(0b0111, im)?)
-                }
-                Token::MovAB => unimplemented!(),
-                Token::MovBA => unimplemented!(),
-                Token::Add(Register::A, im) => unimplemented!(),
-                Token::Add(Register::B, im) => unimplemented!(),
-                Token::Jmp(im) => unimplemented!(),
-                Token::Jnc(im) => unimplemented!(),
-                Token::In(Register::A, im) => unimplemented!(),
-                Token::In(Register::B, im) => unimplemented!(),
-                Token::OutB => unimplemented!(),
-                Token::OutIm(im) => unimplemented!(),
-            }
+            let program = match token {
+                Token::Mov(Register::A, im) => self.gen_bin_code(0b0011, im)?,
+                Token::Mov(Register::B, im) => self.gen_bin_code(0b0111, im)?,
+                Token::MovAB => self.gen_bin_code_with_zero_padding(0b0001),
+                Token::MovBA => self.gen_bin_code_with_zero_padding(0b0100),
+                Token::Add(Register::A, im) => self.gen_bin_code(0b0000, im)?,
+                Token::Add(Register::B, im) => self.gen_bin_code(0b0101, im)?,
+                Token::Jmp(im) => self.gen_bin_code(0b1111, im)?,
+                Token::Jnc(im) => self.gen_bin_code(0b1110, im)?,
+                Token::In(Register::A, im) => self.gen_bin_code(0b0010, im)?,
+                Token::In(Register::B, im) => self.gen_bin_code(0b0110, im)?,
+                Token::OutB => self.gen_bin_code_with_zero_padding(0b1001),
+                Token::OutIm(im) => self.gen_bin_code(0b1011, im)?,
+            };
+            result.push(program);
         }
 
         Ok(result)
@@ -43,13 +40,19 @@ impl Compiler {
             & 0x0f;
         Ok(shift_op | shift_data)
     }
+
+    fn gen_bin_code_with_zero_padding(&self, op: u8) -> u8 {
+        let shift_op = op << 4;
+        let zero_padding = 0b0000 & 0x0f;
+        shift_op | zero_padding
+    }
 }
 
 #[cfg(test)]
 mod compiler_tests {
     use crate::compiler::Compiler;
-    use crate::token::Token::Mov;
     use crate::token::Register;
+    use crate::token::Token::Mov;
 
     #[test]
     fn test_compile_mov_a() {
